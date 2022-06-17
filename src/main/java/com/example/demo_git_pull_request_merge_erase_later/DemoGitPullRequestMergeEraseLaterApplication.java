62799ca2b3006bc2b9b155925906796e3be8b83d
lgcirilo <lgcirilo@gmail.com>
2022-06-17 12:13:44 -0700

    stater.

---
+++ src/main/java/com/example/demo_git_pull_request_merge_erase_later/DemoGitPullRequestMergeEraseLaterApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_git_pull_request_merge_erase_later;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitPullRequestMergeEraseLaterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGitPullRequestMergeEraseLaterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo_git_pull_request_merge_erase_later/DemoGitPullRequestMergeEraseLaterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_git_pull_request_merge_erase_later;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitPullRequestMergeEraseLaterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
